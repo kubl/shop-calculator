3e67045d5d1d3577829fdd53d3a80a9be611bcab
akublitski <aleksandr.kublitskiy@gmail.com>
2022-05-05 22:01:22 +0300

    Initial commit

---
+++ src/main/java/by/kubl/shopcalculator/ShopcalculatorApplication.java
@@ -0,0 +1,13 @@
+package by.kubl.shopcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopcalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopcalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/kubl/shopcalculator/ShopcalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package by.kubl.shopcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopcalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
